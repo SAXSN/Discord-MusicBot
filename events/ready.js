9c51a75e5f306b550ad3da261cba3644b3791a19
SAXSN <79735434+SAXSN@users.noreply.github.com>
2021-02-27 09:55:00 +0530

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,6 +1,7 @@
 module.exports = async (client) => {
   console.log(`[API] Logged in as ${client.user.username}`);
-  await client.user.setActivity("Music", {
+  await client.user.setActivity("+24/7", {
     type: "LISTENING",//can be LISTENING, WATCHING, PLAYING, STREAMING
   });
 };
+
